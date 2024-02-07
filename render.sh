4e4ae58a8449e64f38a371793fd2a945f3e0766e
affengeist <124436782+affengeist@users.noreply.github.com>
2024-02-07 08:58:53 +0100

    Add error handling
    In the previous version, the website directory was always deleted if there was data in the directory. This is a problem if the quarto render command fails, because in this case no new data is available and the old data is deleted.
    
    In the new version, the shell aborts further execution as soon as a command fails and should ideally display a short message.

--- render.sh
+++ render.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Purpose: Render website in container
 # Author: Valentin Ziel
 # Folder check from https://stackoverflow.com/questions/91368/checking-from-shell-script-if-a-directory-contains-files
 # 
 
+# when a command fails, bash exits instead of continuing with the rest of the script.
+set -o errexit
+
 WEBSITE_FOLDER="website/"
 
 # -------------------------------------------------------------
 echo "start rendering website files (html default)"
-quarto render
+quarto render && echo "Quarto render succeeded" || echo "Quarto render failed"
 
 # -------------------------------------------------------------
 files=$(shopt -s nullglob dotglob; echo $WEBSITE_FOLDER*)
